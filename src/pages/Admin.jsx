1ae927b6d57a2ab31c7191164fe1576e42673f4a
andersbroggerlassen-VGB <andersbroggerlassen@gmail.com>
2026-07-07 09:03:56 +0200

    Fix airhorn panel placement

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -68,10 +68,11 @@ export default function Admin({ state }) {
         <label>Rød kamera URL<input value={state.cameraRedUrl} onChange={e => setField('cameraRedUrl', e.target.value)} /></label>
         <label>Blå kamera URL<input value={state.cameraBlueUrl} onChange={e => setField('cameraBlueUrl', e.target.value)} /></label>
         <label>Overblik kamera URL<input value={state.cameraOverviewUrl} onChange={e => setField('cameraOverviewUrl', e.target.value)} /></label>
-        <label>Banekort URL<input value={state.m<Panel title="LYDEFFEKTER"><div className="buttons"><button onClick={() => sound('airhorn')}>AIRHORN</button></div></Panel>apUrl} onChange={e => setField('mapUrl', e.target.value)} /></label>
+        <label>Banekort URL<input value={state.mapUrl} onChange={e => setField('mapUrl', e.target.value)} /></label>
       </Panel>
       <Panel title="LYDEFFEKTER"><div className="buttons"><button onClick={() => sound('siren')}>SIRENE</button><button onClick={() => sound('alarm')}>ALARM</button><button onClick={() => sound('complete')}>MISSION COMPLETE</button><button onClick={() => sound('start')}>START LYD</button></div></Panel>
       <Panel title="SYSTEM"><button className="danger wide" onClick={resetState}>RESET ALT</button></Panel>
     </div>
   </Layout>;
 }
+<Panel title="LYDEFFEKTER"><div className="buttons"><button onClick={() => sound('airhorn')}>AIRHORN</button></div></Panel>
