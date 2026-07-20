cb89d3235ded12217829223a882a95727ceade36
andersbroggerlassen-VGB <andersbroggerlassen@gmail.com>
2026-07-20 20:23:37 +0200

    speak

--- src/lib/audio.js
+++ src/lib/audio.js
@@ -1,13 +1,13 @@
 const sounds = {
   airhorn: '/audio/airhorn.mp3',
-  ready: '/audio/AVA Players get ready.mp3',
-  countdown: '/audio/AVA The battle begins in 321.mp3',
-  greenScore: '/audio/AVA Green team scores.mp3',
-  blueScore: '/audio/AVA Blue team scores.mp3',
-  twoMinutes: '/audio/AVA 2 minutes remaining.mp3',
-  battleEnded: '/audio/AVA Battle has ended.mp3',
-  greenWins: '/audio/AVA The Green team wins.mp3',
-  blueWins: '/audio/AVA The Blue team wins.mp3'
+  ready: '/audio/ready.mp3',
+  countdown: '/audio/countdown.mp3',
+  greenScore: '/audio/green-score.mp3',
+  blueScore: '/audio/blue-score.mp3',
+  twoMinutes: '/audio/two-minutes.mp3',
+  battleEnded: '/audio/battle-ended.mp3',
+  greenWins: '/audio/green-wins.mp3',
+  blueWins: '/audio/blue-wins.mp3'
 };
 
 function play(src) {
@@ -49,8 +49,6 @@ export const playGreenWins = () => play(sounds.greenWins);
 export const playBlueWins = () => play(sounds.blueWins);
 
 export async function playStartSequence() {
-  await playReady();
-  await new Promise(r => setTimeout(r, 1000));
   await playCountdown();
   await playAirhorn();
 }
